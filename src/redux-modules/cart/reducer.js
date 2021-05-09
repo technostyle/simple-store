482a51bf6a5a450d0165f678a5b6d6d1e3e42b48
Danila <perloff.danila@gmail.com>
2021-05-09 22:13:38 +0300

    cart reducer refactoring

--- src/redux-modules/cart/reducer.js
+++ src/redux-modules/cart/reducer.js
@@ -1,5 +1,4 @@
 import {
-  SET_CART_OPEN,
   ADD_PRODUCT_TO_CART,
   CHANGE_PRODUCT_CART_QUANTITY,
 } from "./actions";
@@ -12,26 +11,34 @@ const intialState = {
 
 export const cartReducer = (state = intialState, { type, payload }) => {
   switch (type) {
-    case SET_CART_OPEN:
-      return { ...state, isCartOpen: payload };
     case ADD_PRODUCT_TO_CART:
-      return {
-        ...state,
-        cartProducts: state.cartProducts.concat({ ...payload, quantity: 1 }),
-      };
+      return addProductToCartReducer(state, payload)
     case CHANGE_PRODUCT_CART_QUANTITY:
-      const { name, quantityTerm } = payload;
-      const newCartProducts = state.cartProducts.map((product) => {
-        if (product.name !== name) return product;
-        const newQuantity = Math.max(product.quantity + quantityTerm, 0);
-        if (newQuantity <= 0) return null;
-        return {
-          ...product,
-          quantity: newQuantity,
-        };
-      });
-      return { ...state, cartProducts: newCartProducts.filter(Boolean) };
+      return changeProductsQuantityReducer(state, payload)
     default:
       return state;
   }
 };
+
+function addProductToCartReducer(state, payload) {
+  return {
+    ...state,
+    cartProducts: state.cartProducts.concat({ ...payload, quantity: 1 }),
+  };
+}
+
+function changeProductsQuantityReducer(state, payload) {
+  const { name, quantityTerm } = payload;
+  const newCartProducts = state.cartProducts.map((product) => {
+    if (product.name !== name) return product;
+    const newQuantity = Math.max(product.quantity + quantityTerm, 0);
+    if (newQuantity <= 0) return null;
+
+    return {
+      ...product,
+      quantity: newQuantity,  
+    };
+  });
+
+  return { ...state, cartProducts: newCartProducts.filter(Boolean) };
+}
